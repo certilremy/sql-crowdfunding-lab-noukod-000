e52c4fa31f80034b901b0c344885d191fa94e099
Certil Remy <certil@noukod.com>
2018-11-26 23:47:31 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, name, age) VALUES (1, 'Mackdowel', 17), (2, 'Jean', 6), (3, 'Gerard', 4), (4, 'Samuel', 33),
+(5, 'Ayayy', 24), (6, 'Ou nevem', 24), (7, 'ahish', 9), (8, 'Vitor', 23), (9, 'Frantz', 100), (10, 'Patricia', 30),
+(11, 'Gen problem', 90), (12, 'toujou wi', 24), (13, 'samwela', 4), (14, 'Smithou', 36), (15, 'Albus', 113), (16, 'Squid', 5),
+(17, 'Whale', 6), (18, 'Pcha', 5), (19, 'Elna', 24), (20, 'Kato', 24);
